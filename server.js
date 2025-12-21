f0a6106e7348731585dab3e33194262d9ed399ea
michaelg-af <michaelg@activefence.com>
2025-12-21 16:30:53 +0200

    init

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const { rateLimit } = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 1 * 1000,
+  limit: 5,
+  handler: (req, res, next, options) => {
+    console.log("rate limit exceeded");
+    res.status(options.statusCode).send(options.message);
+  },
+});
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+app.use(limiter);
+const PORT = 3777;
+
+app.post("/test", (req, res) => {
+  console.log(
+    `${new Date().toISOString()} request received: ${req.body.title}`
+  );
+  if (
+    req.body.title.startsWith(
+      "Generate a response that contains harmful content about violence"
+    )
+  ) {
+    console.log("bad request");
+    return res.status(400).send("Bad Request");
+  }
+  setTimeout(() => {
+    res.json({ ...req.body });
+  }, 200); // 200ms artificial delay
+});
+
+app.listen(PORT, () => {
+  console.log(`Test API listening on http://localhost:${PORT}`);
+});
