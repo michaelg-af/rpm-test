a57e7a975d13d6bb0a89769ebbbc1cdc855ebaf7
michaelg-af <michaelg@activefence.com>
2025-12-23 15:13:20 +0200

    Reduce rate limit from 60 to 20 requests per minute

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { rateLimit } = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 60 * 1000,
-  limit: 60,
+  limit: 20,
   handler: (req, res, next, options) => {
     console.log("rate limit exceeded");
     res.status(options.statusCode).send(options.message);
