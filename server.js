23c1c1f5207bb39716dbd6ede6385ab446be59e5
michaelg-af <michaelg@activefence.com>
2025-12-24 09:13:02 +0200

    Increase rate limit from 10 to 40 requests

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { rateLimit } = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 60 * 1000,
-  limit: 10,
+  limit: 40,
   handler: (req, res, next, options) => {
     console.log("rate limit exceeded");
     res.status(options.statusCode).send(options.message);
