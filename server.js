7cfc12ef1b3352c05bff9264744e4a256b60adee
michaelg-af <michaelg@activefence.com>
2025-12-24 09:55:01 +0200

    Increase rate limit from 1 to 2 requests

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { rateLimit } = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 1 * 1000,
-  limit: 1,
+  limit: 2,
   handler: (req, res, next, options) => {
     console.log("rate limit exceeded");
     res.status(options.statusCode).send(options.message);
